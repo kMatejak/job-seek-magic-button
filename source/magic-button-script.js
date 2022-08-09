3db0d7a22eb7f7486b59094ff197d6bbd759096e
Krzysztof S. Matejak <44339684+kMatejak@users.noreply.github.com>
2022-08-09 13:59:17 +0200

    add support for JustJoinIT; delete support for...
    pracuj.pl, praca.pl, olx.pl, rocketjobs.pl

--- source/magic-button-script.js
+++ source/magic-button-script.js
@@ -33,28 +33,10 @@ function copyToClipboard (text) {
     return jobTitle + ";" + companyName;
   }
 
-  function copyFromPracujPl() {
-    let jobTitle = document.getElementsByClassName("OfferView1Z5qAH")[0].innerText;
-    let companyName = document.getElementsByClassName("OfferViewFf0I7D")[0].childNodes[0].nodeValue;
-    return jobTitle + ";" + companyName
-  }
-
-  function copyFromPracaPl() {
-    let jobTitle = document.getElementsByClassName("app-offer__title")[0].innerText;
-    let companyName = document.getElementsByClassName("app-offer__profile-link")[0].innerText;
-    return jobTitle + ";" + companyName
-  }
-
-  function copyFromOLX() {
-    let jobTitle = document.getElementsByClassName("css-1oarkq2-Text eu5v0x0")[0].innerText;
-    let companyName = " ";
-    return jobTitle + ";" + companyName
-  }
-
-  function copyFromRocketJobs() {
-    let jobTitle = document.getElementsByClassName("title")[0].innerText;
-    let companyName = document.getElementsByClassName("tile company-name")[0].getElementsByClassName("value")[0].innerText;
-    return jobTitle + ";" + companyName
+  function copyFromJustJoinIT() {
+    const jobTitle = document.getElementsByClassName('css-1id4k1')[0].innerText;
+    const companyName = document.getElementsByClassName('css-l4opor')[0].innerText;
+    return jobTitle + ";" + companyName;
   }
 
   function multiCopyToClipboard() {
@@ -69,17 +51,8 @@ function copyToClipboard (text) {
       offerData = copyFromLinkedInOfferPage();
       offerURL = window.location.href.substring(0,46);
     }
-    else if (offerURL.substring(12,20) == "praca.pl") {
-      offerData = copyFromPracaPl();
-    }
-    else if (offerURL.substring(12,21) == "pracuj.pl") {
-      offerData = copyFromPracujPl();
-    }
-    else if (offerURL.substring(8,18) == "www.olx.pl") {
-      offerData = copyFromOLX();
-    }
-    else if (offerURL.substring(8,29) == "rocketjobs.pl/offers/") {
-      offerData = copyFromRocketJobs();
+    else if (offerURL.substring(8,27) == "justjoin.it/offers/") {
+      offerData = copyFromJustJoinIT();
     }
     const textToCopy = currentDate + ";" + offerData + ";" + offerURL + ";";
     copyToClipboard(textToCopy);
